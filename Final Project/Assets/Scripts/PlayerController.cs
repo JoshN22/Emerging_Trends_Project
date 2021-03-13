936f16ed41daaf3b32fc85d670c1ed37d4a85cfc
JoshN22 <j.w.newman@hotmail.com>
2021-03-13 19:42:56 -0330

    Created PlayerController script
    Added the PlayerController script onto the player so they can move left to right within range.

---
+++ Final Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 15.0f;
+    public float zRange = 6.57f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Makes the player stop if it goes to far to the left
+        if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+        }
+
+        // Makes the player stop if it goes to far to the right
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+
+        // Allows the user to move left or right 
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+}
