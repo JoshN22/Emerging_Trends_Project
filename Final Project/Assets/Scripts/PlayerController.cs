31946502bef4652a541c6a7c7e68dafd2f199715
JoshN22 <j.w.newman@hotmail.com>
2021-03-14 15:02:47 -0230

    Infinite level and player jump added
    Added functionality to allow the level to be infinite, Player can now jump

--- Final Project/Assets/Scripts/PlayerController.cs
+++ Final Project/Assets/Scripts/PlayerController.cs
@@ -6,30 +6,42 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 15.0f;
-    public float zRange = 6.57f;
+    public float zRangeLeft = 5.4f;
+    public float zRangeRight = -6.6f;
+    public LayerMask groundlayers;
+    public float jumpForce = 7;
+    private Rigidbody rb;
+    public BoxCollider collider;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        collider = GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // Makes the player stop if it goes to far to the left
-        if (transform.position.z < -zRange)
+        if (transform.position.z < zRangeRight)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRangeRight);
         }
 
         // Makes the player stop if it goes to far to the right
-        if (transform.position.z > zRange)
+        if (transform.position.z > zRangeLeft)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRangeLeft);
         }
 
         // Allows the user to move left or right 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
     }
 }
--- Final Project/Assets/Scripts/SpawnManager.cs
+++ Final Project/Assets/Scripts/SpawnManager.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject bg;
-    private float spawnPosY = 30;
-    private float spawnPosX = 30;
-    private float spawnInterval = 4.0f;
-    private float startDelay = 4.0f;
+    private float spawnPosY = 4;
+    private float spawnPosX = 265.9f;
+    private float spawnPosZ = 11;
+    private float spawnInterval = 12.7199f;
+    
 
         // Start is called before the first frame update
     void Start()
     {
-        Invoke("SpawnBackground", startDelay);
+        Invoke("SpawnBackground", 0);
     }
 
     // Spawn random ball at random x position at top of play area
@@ -21,7 +22,7 @@ public class SpawnManager : MonoBehaviour
     {
         
         // Generate random ball index and random spawn position
-        Vector3 spawnPos = new Vector3(spawnPosX, spawnPosY, 0);
+        Vector3 spawnPos = new Vector3(spawnPosX, spawnPosY, spawnPosZ);
         // instantiate ball at random spawn location
         Instantiate(bg, spawnPos, bg.transform.rotation);
         Invoke("SpawnBackground", spawnInterval);
