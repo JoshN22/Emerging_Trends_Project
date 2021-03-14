21b55e825220c1e289de754d31129c38085555ce
tobinman <tylerjtobin@hotmail.com>
2021-03-13 20:34:23 -0330

    Added player movement.
    Added player movement. Working on Repeating Background.

---
+++ Final Project/Assets/Scripts/MoveLevel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLevel : MonoBehaviour
+{
+    private PlayerController playerControllerScript;
+    public float speed = 15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+
+    }
+}
