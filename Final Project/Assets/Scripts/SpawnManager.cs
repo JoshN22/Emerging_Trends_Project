3ea47d5a85b8a1aa7e423d4f1e2062276960eea3
tobinman <tylerjtobin@hotmail.com>
2021-03-19 14:48:14 -0230

    Added Obstacle Spawns
    added obstacle spawns edited box colliders for all game objects.

--- Final Project/Assets/Scripts/SpawnManager.cs
+++ Final Project/Assets/Scripts/SpawnManager.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject[] obstaclePrefabs;
     public GameObject bg;
     private float spawnPosY = 4;
     private float spawnPosX = 265.9f;
     private float spawnPosZ = 11;
     private float spawnInterval = 12.7199f;
+    private float obstacleSpawnInterval = 4.0f;
     
 
         // Start is called before the first frame update
     void Start()
     {
         Invoke("SpawnBackground", 0);
+        InvokeRepeating("SpawnObjects", 2.0f,obstacleSpawnInterval);
     }
 
     // Spawn random ball at random x position at top of play area
@@ -28,6 +31,22 @@ public class SpawnManager : MonoBehaviour
         Invoke("SpawnBackground", spawnInterval);
     }
 
+
+    //spawn obstacles 
+    void SpawnObjects()
+    {
+        // Set random spawn location and random object index
+        Vector3 spawnLocation = new Vector3(130,0,Random.Range(-5.4f, 5.4f));
+        int index = Random.Range(0, obstaclePrefabs.Length);
+        Instantiate(obstaclePrefabs[index], spawnLocation, obstaclePrefabs[index].transform.rotation);
+        
+
+        // If game is still active, spawn new object
+        //if (playerControllerScript.gameOver == false)
+       // {
+           // Instantiate(objectPrefabs[index], spawnLocation, objectPrefabs[index].transform.rotation);
+        //}
+    }
 }
 
 
